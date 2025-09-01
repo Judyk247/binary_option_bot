0032311e1f38c157eeb0ceef3234591f3940efb4
Judyk247 <judechukwunalua@gmail.com>
2025-09-01 06:09:12 +0100

    credentials.py

--- credentials.py
+++ credentials.py
@@ -1,9 +1,10 @@
 # credentials.py
 
-
+# Pocket Option credentials
 sessionToken = "b651d1bb804319e18d46104a66f13197"
 uid = "107618624"
 ACCOUNT_URL = "cabinet"
 
-# If any of the above are missing, raise an error               if not sessionToken or not uid or not ACCOUNT_URL:
+# Ensure credentials are set
+if not sessionToken or not uid:
     raise EnvironmentError("Pocket Option credentials are not set in credentials.py")
