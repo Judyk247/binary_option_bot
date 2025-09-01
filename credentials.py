9db95f6a932f21d785e98778bd8a84ec8ecc39b5
Judyk247 <judechukwunalua@gmail.com>
2025-09-01 05:52:21 +0100

    credentials.py

--- credentials.py
+++ credentials.py
@@ -1,14 +1,9 @@
-import os
+# credentials.py
 
-POCKET_SESSION_TOKEN = os.getenv("POCKET_SESSION_TOKEN")
-POCKET_USER_ID = os.getenv("POCKET_USER_ID")
 
-# Always use live/real mode
-POCKET_ACCOUNT_MODE = "real"
-POCKET_ACCOUNT_URL = "cabinet/real-quick-high-low"
+sessionToken = "b651d1bb804319e18d46104a66f13197"
+uid = "107618624"
+ACCOUNT_URL = "cabinet"
 
-# Validate
-if not POCKET_SESSION_TOKEN or not POCKET_USER_ID:
-    raise EnvironmentError(
-        "Please set POCKET_SESSION_TOKEN and POCKET_USER_ID in your environment variables (.env)"
-    )
+# If any of the above are missing, raise an error               if not sessionToken or not uid or not ACCOUNT_URL:
+    raise EnvironmentError("Pocket Option credentials are not set in credentials.py)
